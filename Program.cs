24814152585a060724fa2a01e1f3a50175816c00
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-07 20:25:09 -0300

    Solução do exercício Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.ForegroundColor= ConsoleColor.DarkGreen;
+            Console.WriteLine("FOGO!");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
